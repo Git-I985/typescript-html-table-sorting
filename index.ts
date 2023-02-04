342579ab483359dcbaf9e900669a89a4e3641f45
Git-I985 <chateauamboise15@gmail.com>
2023-02-04 16:14:52 +0300

    1 file modified

--- index.ts
+++ index.ts
@@ -2,8 +2,8 @@ import { Comparator, initTableSorting, noSort } from './sorting';
 import './style.css';
 
 const firstColumn: Comparator = (row1, row2, order) =>
-  parseInt((order ? row1 : row2).cells[2].innerText) -
-  parseInt((order ? row2 : row1).cells[2].innerText);
+  parseInt((order ? row1 : row2).cells[0].innerText) -
+  parseInt((order ? row2 : row1).cells[0].innerText);
 
 const secondColumn: Comparator = (row1, row2, order) =>
   parseInt((order ? row1 : row2).cells[2].innerText) -
