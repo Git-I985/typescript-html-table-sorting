138b794ef5355363bfe552c44f2617b50d8d7920
Git-I985 <chateauamboise15@gmail.com>
2023-02-04 16:11:47 +0300

    pretty api usage

--- index.ts
+++ index.ts
@@ -2,14 +2,12 @@ import { Comparator, initTableSorting, noSort } from './sorting';
 import './style.css';
 
 const firstColumn: Comparator = (row1, row2, order) =>
-  order
-    ? parseInt(row1.cells[0].innerText) - parseInt(row2.cells[0].innerText)
-    : parseInt(row2.cells[0].innerText) - parseInt(row1.cells[0].innerText);
+  parseInt((order ? row1 : row2).cells[2].innerText) -
+  parseInt((order ? row2 : row1).cells[2].innerText);
 
 const secondColumn: Comparator = (row1, row2, order) =>
-  order
-    ? parseInt(row1.cells[2].innerText) - parseInt(row2.cells[2].innerText)
-    : parseInt(row2.cells[2].innerText) - parseInt(row1.cells[2].innerText);
+  parseInt((order ? row1 : row2).cells[2].innerText) -
+  parseInt((order ? row2 : row1).cells[2].innerText);
 
 const columnsSorting = [firstColumn, noSort, secondColumn];
 
