b495b7c860f875a25a5d8e08c6c86a29ec859476
Git-I985 <chateauamboise15@gmail.com>
2023-02-04 15:53:17 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# typescript-html-table-sorting
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-qhvnno)
---
+++ index.ts
@@ -0,0 +1,80 @@
+import './style.css';
+
+const log = console.log;
+
+type Order = boolean;
+
+type ColumnIndex = number;
+
+type TableRow = HTMLTableRowElement;
+
+type Comparator = (row1: TableRow, row2: TableRow, order: Order) => number;
+
+type TableSorter = (columnIndex: ColumnIndex, order: Order) => void;
+
+type TableHeaderClickHandlerCallback = (
+  tableColumnHeaderIdx: ColumnIndex,
+  order: Order
+) => any;
+
+type TableHeaderClickHandlerReturn = (e: MouseEvent) => void;
+
+const sortRows = (
+  rows: Iterable<TableRow>,
+  order: Order,
+  comparator: Comparator
+): TableRow[] => Array.from(rows).sort((a, b) => comparator(a, b, order));
+
+const createTableSortHeaderClickHandler = (
+  callback: TableHeaderClickHandlerCallback
+): TableHeaderClickHandlerReturn => {
+  let order: Order = true;
+
+  return (e) => {
+    const tableColumnHeader = e.target as HTMLTableCellElement;
+
+    const tableColumnHeaderIdx: ColumnIndex = Array.from(
+      tableColumnHeader.parentNode.children
+    ).indexOf(tableColumnHeader);
+
+    order = !order;
+
+    callback(tableColumnHeaderIdx, order);
+  };
+};
+
+const createTableSorter =
+  (
+    table: HTMLTableElement | HTMLTableSectionElement,
+    comparators: Comparator[]
+  ): TableSorter =>
+  (columnIndex, order) => {
+    const rows = table.querySelectorAll('tr');
+    sortRows(rows, order, comparators[columnIndex]).forEach((row) => {
+      table.appendChild(row);
+    });
+  };
+
+const initTableSorting = (
+  table: HTMLTableElement,
+  comparators: Comparator[]
+) => {
+  const tableHeaders = table.querySelectorAll('thead th');
+  const sortByColumn = createTableSorter(table, comparators);
+  tableHeaders.forEach((tableHeader) => {
+    tableHeader.addEventListener(
+      'click',
+      createTableSortHeaderClickHandler(sortByColumn)
+    );
+  });
+};
+
+const comparators: Comparator[] = [
+  (row1, row2, order) => {
+    return order
+      ? parseInt(row1.cells[0].innerText) - parseInt(row2.cells[0].innerText)
+      : parseInt(row2.cells[0].innerText) - parseInt(row1.cells[0].innerText);
+  },
+];
+
+initTableSorting(document.querySelector('table'), comparators);
